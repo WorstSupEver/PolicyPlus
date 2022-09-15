8fcfaf67edf81acb346a9c20d6f533bdfe070b7c
WorstSupEver <vanya_lukyanov_2015@mail.ru>
2022-09-15 13:43:31 +0300

    Update Version.vb

--- PolicyPlus/Version.vb
+++ PolicyPlus/Version.vb
@@ -1,4 +1,5 @@
-﻿' DO NOT MODIFY THIS FILE. It will be overwritten by version.bat.
-Module VersionHolder
-    Public Const Version As String = ""
-End Module
+' DO NOT MODIFY THIS FILE. To update it, run version.bat again. 
+Module VersionHolder 
+    Public Const Version As String = " 
+" 
+End Module 
